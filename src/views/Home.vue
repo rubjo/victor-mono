5b17666d7adf5658318ad8da8fcf26fe9e951047
Rune Bjørnerås <rune@proweb.no>
2019-05-10 10:14:24 +0200

    setup

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    The
+    <span class="property" />
+    programming font
+  </div>
+</template>
+
+<script>
+import Typed from 'typed.js'
+
+export default {
+  name: 'Home',
+  mounted () {
+    var options = {
+      strings: [
+        'friendly',
+        'elegant',
+        'professional',
+        'crisp',
+        'slender',
+        'consistent',
+        'fancy',
+        'nice',
+        'cool',
+        'must-have',
+        'anti-Trumpian',
+        'scannable',
+        'readable',
+        'enjoyable',
+        'lovely',
+        'effective',
+        'perfect',
+        'ideal'],
+      startDelay: 1000,
+      typeSpeed: 75,
+      backSpeed: 20,
+      smartBackspace: true,
+      backDelay: 2000,
+      shuffle: true
+    }
+
+    var typed = new Typed('.property', options)
+  }
+}
+</script>
